a5ce17ede99f24699d6b172cc6fa9c3d88d1e827
Ulrich Jansen <ulrich.jansen@rwth-aachen.de>
2020-05-07 14:39:38 +0200

    music scaffold

--- code/music/urls.py
+++ code/music/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='music-index'),
+    path('browse', views.browse, name='music-browse'),
+    path('playlist', views.playlist, name='music-playlist'),
+    path('playlists', views.playlists, name='music-playlists'),
+    path('search', views.search, name='music-search'),
+    path('smart', views.smart, name='music-smart'),
 #    path('ajax/cmd/', views.cmd_ajax, name='cmd_ajax'),
 ]
--- code/music/views.py
+++ code/music/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 
 def index(request, mpc):
     return render(request, 'music/index.pug')
+
+
+def browse(request, mpc):
+    return render(request, 'music/browse.pug')
+
+
+def playlist(request, mpc):
+    return render(request, 'music/playlist.pug')
+
+
+def playlists(request, mpc):
+    return render(request, 'music/playlists.pug')
+
+
+def search(request, mpc):
+    return render(request, 'music/search.pug')
+
+
+def smart(request, mpc):
+    return render(request, 'music/smart.pug')
